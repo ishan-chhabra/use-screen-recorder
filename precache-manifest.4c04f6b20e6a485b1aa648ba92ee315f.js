213850cb97930bb6f9669a04667f58c2a5851417
Ishan Chhabra <me@ishanchhabra.com>
2021-06-08 01:51:56 +0530

    Updates

---
+++ precache-manifest.4c04f6b20e6a485b1aa648ba92ee315f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cb2c40c00e6e85950bc56ae7543525",
+    "url": "/use-screen-recorder/index.html"
+  },
+  {
+    "revision": "a5d1ee5a825651ecbc79",
+    "url": "/use-screen-recorder/static/css/main.0791bd9d.chunk.css"
+  },
+  {
+    "revision": "371e7674530ef8ab162b",
+    "url": "/use-screen-recorder/static/js/2.0bcd3267.chunk.js"
+  },
+  {
+    "revision": "4c3f2c95b1f604f960989c03b7df03c0",
+    "url": "/use-screen-recorder/static/js/2.0bcd3267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d1ee5a825651ecbc79",
+    "url": "/use-screen-recorder/static/js/main.1ad2b179.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/use-screen-recorder/static/js/main.1ad2b179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5222436dd34ee8b060a",
+    "url": "/use-screen-recorder/static/js/runtime-main.a60d2e96.js"
+  }
+]);
